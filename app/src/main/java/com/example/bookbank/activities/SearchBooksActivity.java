810d542adc0b4d95f75512cf0dc81dc4268894f4
Mohit <mohit56779@gmail.com>
2020-10-31 22:40:33 -0600

    Basic SearchBooks Activity without firebase added

--- app/src/main/java/com/example/bookbank/activities/SearchBooksActivity.java
+++ app/src/main/java/com/example/bookbank/activities/SearchBooksActivity.java
@@ -3,8 +3,12 @@ package com.example.bookbank.activities;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.bookbank.R;
+import com.example.bookbank.adapters.SearchBooksAdapter;
+import com.example.bookbank.models.BookSearch;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -17,11 +21,34 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.ArrayList;
+
 public class SearchBooksActivity extends AppCompatActivity {
+
+    // Declaring the variables needed
+    ListView searchList;
+    ArrayAdapter<BookSearch> bookAdapter;
+    ArrayList<BookSearch> bookArrayList;
+
+    SearchBooksAdapter searchBooksAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_books);
+
+        searchList = findViewById(R.id.search_list);
+
+        bookArrayList = new ArrayList<>();
+
+        bookArrayList.add(new BookSearch("1","2","3","4","5","6"));
+
+        bookAdapter = new SearchBooksAdapter(this,R.layout.search_book_content , bookArrayList);
+
+        searchList.setAdapter(bookAdapter);
+
+
+
     }
 }
