2a23cca03d37c852b3736fef79b2d429094b43c0
Victor N <victor3@ualberta.ca>
2020-11-12 16:28:51 -0700

    Permission for location is asked in SetLocationActivity()

--- app/src/main/java/com/example/bookbank/activities/SetLocationActivity.java
+++ app/src/main/java/com/example/bookbank/activities/SetLocationActivity.java
@@ -1,13 +1,21 @@
 package com.example.bookbank.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.bookbank.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -15,17 +23,83 @@ public class SetLocationActivity extends FragmentActivity implements OnMapReadyC
 
     private GoogleMap mMap;
 
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 01;
+    private Boolean mLocationPermissionsGranted = false; //By default
+
+    private static final  String TAG = "MAP";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_location);
 
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-       // SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-       //         .findFragmentById(R.id.map);
-       // mapFragment.getMapAsync(this);
+        getLocationPermission();
     }
+
+    /**
+     *
+     */
+    private  void initMap(){
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        Log.d(TAG, "Initializing Map!");
+        // Map ID
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Check if Location access is allowed by user
+     */
+    private void getLocationPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COURSE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionsGranted = true;
+            }
+        }
+        else{
+            // Ask user for permission
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    /**
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+       // super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResult() is called!");
+        mLocationPermissionsGranted = false;
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if (grantResults.length > 0){
+                    for (int i = 0; i < grantResults.length; i++){
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionsGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult() failed!");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult() granted!");
+                    mLocationPermissionsGranted = true;
+                    initMap(); //Start initialing the map
+                }
+            }
+        }
+
+    }
+
+
 
 
 
@@ -40,6 +114,8 @@ public class SetLocationActivity extends FragmentActivity implements OnMapReadyC
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is ready!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Map is ready!");
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
