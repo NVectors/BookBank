3f74044c1dec495dec2bb2ed47248f38b103c007
Victor N <victor3@ualberta.ca>
2020-11-12 19:55:58 -0700

    Get current location in SetLocationActivity()

--- app/src/main/java/com/example/bookbank/activities/SetLocationActivity.java
+++ app/src/main/java/com/example/bookbank/activities/SetLocationActivity.java
@@ -2,6 +2,7 @@ package com.example.bookbank.activities;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -12,20 +13,27 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.bookbank.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class SetLocationActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private FusedLocationProviderClient mFusedLocationProviderCLient;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 01;
     private Boolean mLocationPermissionsGranted = false; //By default
-
-    private static final  String TAG = "MAP";
+    private static final float DEFAULT_ZOOM = 15f;
+    private static final String TAG = "MAP";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +45,7 @@ public class SetLocationActivity extends FragmentActivity implements OnMapReadyC
     /**
      *  Initialize the Map fragment
      */
-    private  void initMap(){
+    private void initMap() {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         Log.d(TAG, "Initializing Map!");
         // Map ID
@@ -45,16 +53,48 @@ public class SetLocationActivity extends FragmentActivity implements OnMapReadyC
         mapFragment.getMapAsync(this);
     }
 
-    private void getDeviceLocation(){
+    private void getDeviceLocation() {
         Log.d(TAG, "Getting the device location");
 
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
+        try {
+            if (mLocationPermissionsGranted) {
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Found Location!");
+                            Location currentLocation = (Location) task.getResult();
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+                        } else {
+                            Log.d(TAG, "Current location is null!");
+                            Toast.makeText(SetLocationActivity.this, "Unable to get Current Location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "Security Exception!");
+        }
+    }
+
+    /**
+     *
+     * @param latLng
+     * @param zoom
+     */
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "Moving camera to: Lat: " + latLng.latitude + ", Lng: " + latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 
     /**
      * Check if Location permission access is allowed by user
      */
-    private void getLocationPermission(){
+    private void getLocationPermission() {
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION};
 
@@ -65,8 +105,7 @@ public class SetLocationActivity extends FragmentActivity implements OnMapReadyC
                 mLocationPermissionsGranted = true;
                 initMap();
             }
-        }
-        else{
+        } else {
             // Ask user for permission
             ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
         }
@@ -80,14 +119,14 @@ public class SetLocationActivity extends FragmentActivity implements OnMapReadyC
      */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-       // super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         Log.d(TAG, "onRequestPermissionsResult() is called!");
         mLocationPermissionsGranted = false;
-        switch(requestCode){
-            case LOCATION_PERMISSION_REQUEST_CODE:{
-                if (grantResults.length > 0){
-                    for (int i = 0; i < grantResults.length; i++){
-                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             mLocationPermissionsGranted = false;
                             Log.d(TAG, "onRequestPermissionsResult() failed!");
                             return;
@@ -105,8 +144,7 @@ public class SetLocationActivity extends FragmentActivity implements OnMapReadyC
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -116,6 +154,26 @@ public class SetLocationActivity extends FragmentActivity implements OnMapReadyC
         Toast.makeText(this, "Map is ready!", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "Map is ready!");
         mMap = googleMap;
+
+        if (mLocationPermissionsGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mMap.setMyLocationEnabled(true); // Set blue marker of where current location is
+            mMap.getUiSettings().setMyLocationButtonEnabled(false); // Remove button to go back to current location
+            //mMap.getUiSettings().setCompassEnabled(true);
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+            mMap.getUiSettings().setRotateGesturesEnabled(true);
+        }
 
     }
 }
